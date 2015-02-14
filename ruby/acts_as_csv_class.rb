fdba6fdd6ff3eaa3a407d7bc344d2705ec57fd3e
CodingAnarchy <mtanous22@gmail.com>
2015-02-13 19:11:51 -0700

    Change CSV parsing class to use acts_as_csv macro

--- ruby/acts_as_csv_class.rb
+++ ruby/acts_as_csv_class.rb
@@ -1,28 +1,31 @@
 class ActsAsCsv
-	def read
-		file = File.new(self.class.to_s.downcase + '.txt')
-		@headers = file.gets.chomp.split(', ')
+	def self.acts_as_csv
+		def read
+			file = File.new(self.class.to_s.downcase + '.txt')
+			@headers = file.gets.chomp.split(', ')
 
-		file.each do |row|
-			@result << row.chomp.split(', ')
+			file.each do |row|
+				@result << row.chomp.split(', ')
+			end
 		end
-	end
 
-	def headers
-		@headers
-	end
+		def headers
+			@headers
+		end
 
-	def csv_contents
-		@result
-	end
+		def csv_contents
+			@result
+		end
 
-	def initialize
-		@result = []
-		read
+		def initialize
+			@result = []
+			read
+		end
 	end
 end
 
 class RubyCsv < ActsAsCsv
+	acts_as_csv
 end
 
 m = RubyCsv.new
